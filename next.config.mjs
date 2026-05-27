10b332a4b7b47bfa1b0f8c991464cb21d2cb71f7
oskarfuchs1508-boop <oskarfuchs1508@gmail.com>
2026-05-27 15:20:04 +0200

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      { protocol: 'https', hostname: 'cdn.sanity.io' },
-    ],
-  },
-};
-
+const nextConfig = {};
 export default nextConfig;
